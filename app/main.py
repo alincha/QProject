c567b88ea489b1cbb6d386c0c1d1bff2f4b6b244
alincha <k.alincha@gmail.com>
2022-12-12 01:01:33 +0400

    Now main.py uses command line arguments. Tests in main.py are commented.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 import data.create
-import tests
+import sys
+import script
 
 if __name__ == '__main__':
-    Session = data.create.init_db()
+    args = sys.argv[1:]
+    if len(args) != 2:
+        exit(1)
+    op, arg = args
 
-    tests.test_convert_dna_to_rna(Session)
-    tests.test_convert_rna_to_protein(Session)
+    Session = data.create.init_db()
+    if op == 'transcribe':
+        print(script.convert_dna_to_rna(Session, arg))
+    elif op == 'translate':
+        print(script.convert_rna_to_protein(Session, arg))
+    else:
+        exit(1)
+
+    # tests.test_convert_dna_to_rna(Session)
+    # tests.test_convert_rna_to_protein(Session)
